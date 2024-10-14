7b211637a5a50156c91b2510ac625c1e8adeb78c
DanielOspina2301 <daniel.alberto.ospina@gmail.com>
2024-10-14 16:46:05 -0500

    Add gcp and api logic

---
+++ api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from google.cloud import bigquery
+
+from src.queries.queries import top_dates_query
+
+app = FastAPI()
+
+# Initialize bigquery client
+client = bigquery.Client.from_service_account_json("/Users/lauraramirez/Downloads/latam-challenge-438317-ff0b05c25930.json")
+
+
+@app.get("/top_dates")
+async def query_top_dates():
+
+    try:
+        # Execute query
+        query_job = client.query(top_dates_query)
+
+        # Get results
+        results = query_job.result()  # Espera a que la consulta se complete
+
+        # Convert to list
+        data = [dict(row) for row in results]
+
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
